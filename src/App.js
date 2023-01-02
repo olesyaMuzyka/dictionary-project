62ccc091a69f0693092287f604c4b51b64cdc383
olesyaMuzyka <muzyka.olesya@gmail.com>
2023-01-02 19:18:52 +0900

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <button type="button" className="btn btn-primary">
-          Search
-        </button>
-      </header>
+      <div className="container">
+        <header className="app-header">
+          <h1>DICTIONARY</h1>
+          <h2>Enhance Your Vocabulary</h2>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          <a
+            href="https://github.com/olesyaMuzyka/dictionary-project"
+            target="blank"
+            rel="noreferrer"
+          >
+            Open-source coded
+          </a>
+          <strong> by Olesya Muzyka</strong>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a word"
+              className="form-control"
+              onChange={handleKeywordChange}
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
