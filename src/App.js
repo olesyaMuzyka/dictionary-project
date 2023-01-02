ebdc282e3641b3a44bab58418e111975e8c80755
olesyaMuzyka <muzyka.olesya@gmail.com>
2023-01-02 16:25:15 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <button type="button" className="btn btn-primary">
+          Search
+        </button>
+      </header>
+    </div>
+  );
+}
