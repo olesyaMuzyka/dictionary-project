e70a28f537ea77b4f2aeabff31f8b7ec839b8497
olesyaMuzyka <muzyka.olesya@gmail.com>
2023-01-04 19:04:05 +0900

    Added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [result, setResult] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
+
+  function handleResponse(response) {
+    setResult(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000012a4c8218b57e48189077cfb2fb2d0cc4";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     search();
-  }
-
-  function handleResponse(response) {
-    setResult(response.data[0]);
   }
 
   function handleKeywordChange(event) {
@@ -58,6 +70,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Result result={result} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-3" key={index}>
+                <img src={photo.src.landscape} className="img-fluid" alt="" />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
