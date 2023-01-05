04ef5ce55c837e344b9dd80c00ab3cf7cc4d2392
olesyaMuzyka <muzyka.olesya@gmail.com>
2023-01-05 10:54:37 +0900

    Revised photos js/css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary(props) {
 
     let pexelsApiKey =
       "563492ad6f917000010000012a4c8218b57e48189077cfb2fb2d0cc4";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
